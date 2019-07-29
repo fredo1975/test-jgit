dac4fe05e7d77c254d5fb1b3254cc88f279f9b68
fredo1975 <fredo@BLUECHIPIT-1>
2019-07-29 20:37:33 +0200

    add syso

--- test-jgit-1/src/main/java/fr/bluechipit/jgit/App.java
+++ test-jgit-1/src/main/java/fr/bluechipit/jgit/App.java
@@ -3,6 +3,7 @@ package fr.bluechipit.jgit;
 public class App {
 	
     public static void main( String[] args ){
-    	
+    	System.out.println("App start");
+    	System.out.println("App end");
     }
 }
